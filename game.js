87cb38d1df71733558047c690e2a383148c0d4c5
ImanPJN <imanpajand@gmail.com>
2025-11-02 01:20:46 +0400

    Update game.js

--- game.js
+++ game.js
@@ -126,7 +126,7 @@
     function initializeGame() {
       var sdkActive = window.FarcadeSDK && window.FarcadeSDK.singlePlayer;
 
-      // ۱. اجرای "Remix Only": اگر SDK فعال نیست، متوقف شو.
+      // Remix Only
       if (!sdkActive) {
         console.error("Farcade SDK not found. Game must run inside the platform.");
         // *اختیاری*: اگر می‌خواهید چیزی نمایش داده نشود، می‌توانید Runner container را مخفی کنید.
